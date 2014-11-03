87f5c3a4b28a4cc32fba3a2ae24757f1dc6dbb61
Praful Mathur <prafmathur@gmail.com>
2014-11-03 14:47:53 -0500

    dividing most common words by stars

--- init.py
+++ init.py
@@ -11,11 +11,11 @@ def build_sample_file(reviews_json):
 def read_n_reviews(n, infile):
     with open(infile) as data:
         reviews_json = [next(data) for i in range(n)]
-    reviews = [json.loads(review)["text"] for review in reviews_json]
+    reviews = [[json.loads(review)["text"], json.loads(review)["stars"]] for review in reviews_json]
     
     # Remove punctuation
     remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-    reviews_without_punctutation = [review.translate(remove_punctuation_map) for review in reviews]
+    reviews_without_punctutation = [[review[0].translate(remove_punctuation_map), review[1]] for review in reviews]
     
     return reviews_without_punctutation
 
@@ -31,15 +31,26 @@ def run():
     custom_stopwords = []
     stopwords = default_stopwords + custom_stopwords
 
-    first_reviews = read_n_reviews(1000, "reviews_sample.json")  
+    first_reviews = read_n_reviews(20000, "yelp_academic_dataset_review.json")
+    reviews_by_stars = {
+      1:[],
+      2:[],
+      3:[],
+      4:[],
+      5:[]
+    }
+
+
     concat_text = []
     for r in first_reviews:
-        concat_text.extend([format_word(word) for word in r.lower().split() if word not in stopwords])
+        reviews_by_stars[r[1]].extend([format_word(word) for word in r[0].lower().split() if word not in stopwords])
+
     # print "---------------ConcatText--------------"
     # print concat_text
-    fdist = FreqDist(concat_text)
-    print "---------------Most Common--------------"
-    print fdist.most_common(100)
+    for key in reviews_by_stars:
+      fdist = FreqDist(reviews_by_stars[key])
+      print "---------------Most Common " + str(key) + " Star Words --------------"
+      print fdist.most_common(100)
 
 
 
