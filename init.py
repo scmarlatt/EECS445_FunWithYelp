610b92a78d82da1b1b1bed979756bbf72dde3411
Praful Mathur <prafmathur@gmail.com>
2014-11-09 21:39:16 -0500

    Naive bayes with 10k reviews

--- init.py
+++ init.py
@@ -17,7 +17,6 @@ def read_n_reviews(n, infile):
     # Remove punctuation
     remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
     reviews_without_punctutation = [[review[0].translate(remove_punctuation_map), review[1]] for review in reviews]
-    
     return reviews_without_punctutation
 
 def format_word(word):
@@ -33,13 +32,13 @@ def load_stop_words():
 
 def common_words_by_stars():
     # Number of reviews to process
-    num_reviews_to_process = 2000
+    num_reviews_to_process = 200000
     #------------------------------
 
     stopwords = load_stop_words()
 
     first_reviews = read_n_reviews(num_reviews_to_process, "yelp_academic_dataset_review.json")
-    reviews_by_stars = {
+    train_reviews_by_star = {
       1:[],
       2:[],
       3:[],
@@ -48,39 +47,53 @@ def common_words_by_stars():
     }
 
     for r in first_reviews:
-        reviews_by_stars[r[1]].extend([word for word in r[0].lower().split() if word not in stopwords])
-    for key in reviews_by_stars:
-        fdist = FreqDist(reviews_by_stars[key])
+        train_reviews_by_star[r[1]].extend([word for word in r[0].lower().split() if word not in stopwords])
+    for key in train_reviews_by_star:
+        fdist = FreqDist(train_reviews_by_star[key])
         print "---------------Most Common " + str(key) + " Star Words --------------"
         print fdist.most_common(100)
 
 def naive_bayes():
     # Number of reviews to process
-    num_reviews_to_process = 5200
+    num_reviews_to_process = 10000 
     #------------------------------
 
     stopwords = load_stop_words()
 
     first_reviews = read_n_reviews(num_reviews_to_process, "yelp_academic_dataset_review.json")
-    reviews_by_stars = {1:[], 2:[], 3:[], 4:[], 5:[]}
-    for r in first_reviews:
-        reviews_by_stars[r[1]].append([word for word in r[0].lower().split() if word not in stopwords])
+    train_reviews_by_star = {1:[], 2:[], 3:[], 4:[], 5:[]}
+    test_reviews_by_star = {1:[], 2:[], 3:[], 4:[], 5:[]}
+
+
+    for idx, r in enumerate(first_reviews):
+        if idx % 5 == 0:
+          test_reviews_by_star[r[1]].append([word for word in r[0].lower().split() if word not in stopwords])
+        else:
+          train_reviews_by_star[r[1]].append([word for word in r[0].lower().split() if word not in stopwords])
+
 
     #-----Implementing Naive Bayes--------
-    train_set = []   
-    for r in reviews_by_stars[1]:
-       features = {} 
-       for word in r:
-           features[word] = True
-       train_set.append((features, "1"))
-    for r in reviews_by_stars[5]:
-       features = {} 
-       for word in r:
-           features[word] = True
-       train_set.append((features, "5"))
-
+    train_set = []
+    for rating, reviews in train_reviews_by_star.iteritems():
+      for r in reviews:
+         features = {} 
+         for word in r:
+             features[word] = True
+         train_set.append((features, str(rating)))
+    
+    test_set = {1:[], 2:[], 3:[], 4:[], 5:[]}
+    for rating, reviews in test_reviews_by_star.iteritems():
+      for r in reviews:
+         features = {} 
+         for word in r:
+             features[word] = True
+         test_set[rating].append((features))
+    
     classifier = nltk.classify.NaiveBayesClassifier.train(train_set)
     print classifier.show_most_informative_features(200)
+
+    for i in range(1,6):
+      print classifier.classify_many(test_set[i]).count(str(i))/float(len(test_set[i]))
 
 def run():
     naive_bayes()
