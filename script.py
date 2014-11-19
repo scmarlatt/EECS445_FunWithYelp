9690be591ebda0ba1d274354f090afd3ad0fc147
karan <karnvish@umich.edu>
2014-11-18 23:55:34 -0500

    checking sci-kit installation

---
+++ script.py
@@ -0,0 +1,9 @@
+
+from sklearn.cross_validation import KFold
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_selection import chi2
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.naive_bayes import MultinomialNB
+import json
+import numpy as np
+import operator
