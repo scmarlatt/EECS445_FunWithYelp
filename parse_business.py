537b9e6176b1d9ec937a0f58a0a76dc12a4d7083
karan <karnvish@umich.edu>
2014-12-10 23:32:27 -0500

    adding restraunts

--- parse_business.py
+++ parse_business.py
@@ -1,13 +1,13 @@
 import json
+from pprint import pprint
 
 def main():
-	f = open('../yelp_academic_dataset_business.json','rb')
+  f = open('../yelp_academic_dataset_business.json','r')
 
-	for line in f
-		rec = json.loads(line.strip())
-		if rec["categories"] == "Restaurants" || rec["categories"] == "Food"
-			print rec["business id"]
-
+  for line in f:
+    rec = json.loads(line.strip())
+    if "Restaurants" in rec["categories"] or "Food" in rec["categories"]:
+      print rec["business_id"]
 
 if __name__ == "__main__":
-        main()
+  main()
