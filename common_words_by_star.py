841cf468a9c22ec7d18e8f89f2aa70d867cabbf4
scmarlatt <smarlatt@umich.edu>
2014-12-13 01:05:00 -0500

    ummm...things

---
+++ common_words_by_star.py
@@ -0,0 +1,19 @@
+import nltk
+
+def get_common_words(reviews, num_wrods):
+	# reviews[1] is a list of 1 star reviews...
+	# Number of reviews to process
+
+	words_in_reviews = {}
+	for i = [1,2,3,4,5]:
+		words_in_reviews[i] = [word for review in reviews[i] for word in review]
+
+	fdists = {}
+	for i = [1,2,3,4,5]:
+		fdists[i] = nltk.FreqDist(words_in_reviews[i])
+	
+	most_common_words_lists = {}
+	for i = [1,2,3,4,5]:
+		most_common_words_lists[i] = fdists[i].most_common(num_words)
+
+	return most_common_words_lists
