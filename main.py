3affd04fbe4750e0b8eaa1f90de6bebf2363147d
scmarlatt <smarlatt@umich.edu>
2014-12-12 23:32:42 -0500

    asdf

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import read_reviews
 
 def main():
 
-	#read reviews
+	reviews = read_reviews.read(10000)
 
 	#load into feature matrix
 
-	regr = general_regression.lin_reg(train_x, train_t)
+	#regr = general_regression.lin_reg(train_x, train_t)
 
-	general_regression.test_and_print_regression(test_x, test_t, regr)
+	#general_regression.test_and_print_regression(test_x, test_t, regr)
 
 
 if __name__ == "__main__":
