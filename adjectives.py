b73751f915157a216c7d51bd6cff395f485eaa82
Praful Mathur <prafmathur@gmail.com>
2014-12-13 18:35:18 -0500

    fixed rishis stuff

--- adjectives.py
+++ adjectives.py
@@ -5,10 +5,14 @@ import sys
 def get_adjectives(reviews):
 	one_star = {}
 	for review in reviews[1]:
-		pos_assign = nltk.pos_tag(review)
-		for assignment in pos_assign:
+                try:
+                    pos_assign = nltk.pos_tag(review)
+                except:
+                    continue
+                for assignment in pos_assign:
 			if assignment[1]=='JJ':
 				if assignment[0] in one_star:
-					one_star[assignment[0]] += 1
-				one_star[assignment[0]] = 1
+				    one_star[assignment[0]] += 1
+                                else:
+                                    one_star[assignment[0]] = 1
 	return one_star
