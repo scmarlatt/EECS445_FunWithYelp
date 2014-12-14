ace7c60b27760aee0a55e2bf9309a4454deb425a
Praful Mathur <prafmathur@gmail.com>
2014-12-14 14:19:00 -0500

    added read_reviews

--- read_reviews.py
+++ read_reviews.py
@@ -1,6 +1,6 @@
 def read_star_reviews(infile, n):
 	with open(infile) as review_input:
-		return [next(review_input).split() for i in range(n)]				
+		return [next(review_input).split() for i in range(n)]
 
 def read(num_each_review):
 	reviews = {}
@@ -9,5 +9,16 @@ def read(num_each_review):
 	reviews[3] = read_star_reviews("star_texts/three_stars.txt", num_each_review)
 	reviews[4] = read_star_reviews("star_texts/four_stars.txt", num_each_review)
 	reviews[5] = read_star_reviews("star_texts/five_stars.txt", num_each_review)
+
+	return reviews
+
+
+def read_useful(num_each_review):
+	reviews = {}
+	reviews[1] = read_star_reviews("useful_reviews/one_stars.txt", num_each_review)
+	reviews[2] = read_star_reviews("useful_reviews/two_stars.txt", num_each_review)
+	reviews[3] = read_star_reviews("useful_reviews/three_stars.txt", num_each_review)
+	reviews[4] = read_star_reviews("useful_reviews/four_stars.txt", num_each_review)
+	reviews[5] = read_star_reviews("useful_reviews/five_stars.txt", num_each_review)
 
 	return reviews
