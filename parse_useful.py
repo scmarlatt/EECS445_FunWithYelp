8c535ad565420e61e587d22b6158d474bfa447de
karan <karnvish@umich.edu>
2014-12-14 03:14:18 -0500

    new review files

--- parse_useful.py
+++ parse_useful.py
@@ -5,50 +5,50 @@ import codecs
 import string
 
 def main():
-	f = open('useful_reviews.json', 'r')
-	clean_review = ""
-	q = open('features_text/stopwords.txt')
-	stopwords = []
-	restaurants = open('features_text/restaurant_ids.txt')
-	ids = []
-	one_stars = codecs.open('one_stars.txt', "w", "utf-8")
-	two_stars = codecs.open('two_stars.txt', "w", "utf-8")
-	three_stars = codecs.open('three_stars.txt', "w", "utf-8")
-	four_stars = codecs.open('four_stars.txt', "w", "utf-8")
-	five_stars = codecs.open('five_stars.txt', "w", "utf-8")
+  f = open('useful_reviews.json', 'r')
+  clean_review = ""
+  q = open('features_text/stopwords.txt')
+  stopwords = []
+  restaurants = open('features_text/restaurant_ids.txt')
+  ids = []
+  one_stars = codecs.open('useful_reviews/one_stars.txt', "w", "utf-8")
+  two_stars = codecs.open('useful_reviews/two_stars.txt', "w", "utf-8")
+  three_stars = codecs.open('useful_reviews/three_stars.txt', "w", "utf-8")
+  four_stars = codecs.open('useful_reviews/four_stars.txt', "w", "utf-8")
+  five_stars = codecs.open('useful_reviews/five_stars.txt', "w", "utf-8")
 
-	for word in restaurants:
-		ids.append(word.rstrip())
-	stemmer = SnowballStemmer("english")
-	for word in stopwords:
-		stopwords.append(word)
+  for word in restaurants:
+    ids.append(word.rstrip())
+  stemmer = SnowballStemmer("english")
+  for word in q:
+    stopwords.append(word.rstrip())
+  for line in f:
+    rec = json.loads(line.strip())
+    if (rec["business_id"] in ids):
+      r = unicode(rec["text"].lower())
+      remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+      r2 = r.translate(remove_punctuation_map)
 
-	for line in f:
-		rec = json.loads(line.strip())
-		if (rec["business_id"] in ids):
-			r = unicode(rec["text"].lower())
-			remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-			r2 = r.translate(remove_punctuation_map)
-
-			arr = r2.split(" ")
-			for i in range(0, len(arr)):
-				if arr[i] in stopwords:
-					arr[i] = ""
-				else:
-			 		arr[i] = stemmer.stem(arr[i])
-			 		continue
-			final = ' '.join(arr)
-
-			if rec["stars"] == 1:
-				one_stars.write(final)
-			elif rec["stars"] == 2:
-				two_stars.write(final)
-			elif rec["stars"] == 3:
-				three_stars.write(final)			
-			elif rec["stars"] == 4:
-				four_stars.write(final)
-			elif rec["stars"] == 5:
-				five_stars.write(final)
+      arr = r2.split(" ")
+      for i in range(0, len(arr)):
+        if arr[i] in stopwords:
+          arr[i] = ""
+        else:
+         #  arr[i] = stemmer.stem(arr[i])
+           continue
+      final = ' '.join(arr)
+      final = final.replace('\n', ' ')
+      final = ' '.join(final.split())
+      if rec["stars"] == 1:
+        one_stars.write(final + '\n')
+      elif rec["stars"] == 2:
+        two_stars.write(final + '\n')
+      elif rec["stars"] == 3:
+        three_stars.write(final + '\n')      
+      elif rec["stars"] == 4:
+        four_stars.write(final + '\n')
+      elif rec["stars"] == 5:
+        five_stars.write(final + '\n')
 
 if __name__ == "__main__":
   main()
