4ea0468924f1cb2ea1243d1ddd4b52548ef45be9
Rishi Zaveri <rishiz@umich.edu>
2014-12-13 21:38:34 -0500

    writing to text files

--- verbs.py
+++ verbs.py
@@ -3,12 +3,12 @@ import nltk.classify
 import sys
 import operator
 
-def write_adjectives(reviews):
-    f1 = open('POS/adj_list1.txt', 'w')
-    f2 = open('POS/adj_list2.txt', 'w')
-    f3 = open('POS/adj_list3.txt', 'w')
-    f4 = open('POS/adj_list4.txt', 'w')
-    f5 = open('POS/adj_list5.txt', 'w')
+def write_verbs(reviews):
+    f1 = open('POS/verb_list1.txt', 'w')
+    f2 = open('POS/verb_list2.txt', 'w')
+    f3 = open('POS/verb_list3.txt', 'w')
+    f4 = open('POS/verb_list4.txt', 'w')
+    f5 = open('POS/verb_list5.txt', 'w')
 
     for review in reviews[1]:
         try:
@@ -16,7 +16,7 @@ def write_adjectives(reviews):
         except:
             continue
         for assignment in pos_assign:
-            if assignment[1]=='JJ':
+            if aassignment[1]=='VBP' or assignment[1]=='VBD':
                 f1.write(assignment[0] + ' ')
 
     for review in reviews[2]:
@@ -25,7 +25,7 @@ def write_adjectives(reviews):
         except:
             continue
         for assignment in pos_assign:
-            if assignment[1]=='JJ':
+            if assignment[1]=='VBP' or assignment[1]=='VBD':
                 f2.write(assignment[0] + ' ')
 
     for review in reviews[3]:
@@ -34,7 +34,7 @@ def write_adjectives(reviews):
         except:
             continue
         for assignment in pos_assign:
-            if assignment[1]=='JJ':
+            if assignment[1]=='VBP' or assignment[1]=='VBD':
                 f3.write(assignment[0] + ' ')
 
     for review in reviews[4]:
@@ -43,7 +43,7 @@ def write_adjectives(reviews):
         except:
             continue
         for assignment in pos_assign:
-            if assignment[1]=='JJ':
+            if assignment[1]=='VBP' or assignment[1]=='VBD':
                 f4.write(assignment[0] + ' ')
 
     for review in reviews[5]:
@@ -52,6 +52,6 @@ def write_adjectives(reviews):
         except:
             continue
         for assignment in pos_assign:
-            if assignment[1]=='JJ':
+            if assignment[1]=='VBP' or assignment[1]=='VBD':
                 f5.write(assignment[0] + ' ')
        
