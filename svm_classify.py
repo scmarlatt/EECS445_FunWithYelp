da2653c2fb59482de802167049cc3a0a71d98808
karan <karnvish@umich.edu>
2014-12-14 20:15:52 -0500

    Fixed classifier bugs

--- svm_classify.py
+++ svm_classify.py
@@ -11,20 +11,13 @@ def classify(train_x, train_t):
 def test_and_print_svm(test_x, test_t, svm):
 
 	predictions = svm.predict(test_x)
-
-	# rounded_predictions = [round(pred, 0) for pred in predictions]
+	test_size = len(test_t)
 
 	num_correct = 0
 	value = 0
 	for i in range(test_size):
-		# temp = predictions[i]
-		# if temp > 5.0:
-		# 	temp = 5.0
-		# if temp < 1.0:
-		# 	temp = 1.0
-		# value += abs(temp - test_t[i])
 		if predictions[i] == test_t[i]:
 			num_correct += 1
 
 	# print value/test_size
-	print "Accuracy: " + str(float(num_correct)/len(test_t)) #accuracy
+	print "Accuracy: " + str(float(num_correct)/test_size) #accuracy
